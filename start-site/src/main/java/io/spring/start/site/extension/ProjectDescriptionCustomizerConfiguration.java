5b9386fe7b4d9687094a39716efa302279d39163
HaiTao Zhang <haitaozhang@HaiTaos-MacBook-Pro.local>
2019-06-17 11:31:04 -0700

    update branch

--- start-site/src/main/java/io/spring/start/site/extension/ProjectDescriptionCustomizerConfiguration.java
+++ start-site/src/main/java/io/spring/start/site/extension/ProjectDescriptionCustomizerConfiguration.java
@@ -44,9 +44,4 @@ public class ProjectDescriptionCustomizerConfiguration {
 		return new GradleDslProjectDescriptionCustomizer();
 	}
 
-	@Bean
-	public MavenCoordinatesProjectDescriptionCustomizer mavenCoordinatesProjectDescriptionCustomizer() {
-		return new MavenCoordinatesProjectDescriptionCustomizer();
-	}
-
 }
